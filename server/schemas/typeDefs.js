b9a02316af49866ccc3eb97d9f5368253352bed9
Blaine Curren <blaine.curren@gmail.com>
2022-12-14 13:46:16 -0600

    added type User to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,6 +2,15 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
 
+type User {
+  _id: ID
+  username: String
+  email: String
+  friendCount: Int
+  thoughts: [Thought]
+  friends: [User]
+}
+
 type Query {
   users: [User]
   user(username: String!): User
