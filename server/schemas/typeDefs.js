d68a678dded8f23a391765190f0d840d6beef5a5
Blaine Curren <blaine.curren@gmail.com>
2022-12-15 05:39:27 -0600

    added mutation to typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,22 +1,14 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-
-type User {
-  _id: ID
-  username: String
-  email: String
-  friendCount: Int
-  thoughts: [Thought]
-  friends: [User]
-}
-
-type Query {
-  users: [User]
-  user(username: String!): User
-  thoughts(username: String) [Thought]
-  thought(_id: ID!): Thought
-}
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
 
   type Thought {
     _id: ID
@@ -35,6 +27,17 @@ type Query {
   }
   type Query {
     thoughts(username: String): [Thought]
+  }
+  type Query {
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): User
+    addUser(username: String!, email: String!, password: String!): User
   }
 `;
 
