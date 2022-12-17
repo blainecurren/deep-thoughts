915f126aa8e52353ee0102613af72466071e3ee5
Blaine Curren <blaine.curren@gmail.com>
2022-12-16 22:27:56 -0600

    reverted npm packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React from "react";
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className='flex-column justify-flex-start min-100-vh'>
+    <div className="flex-column justify-flex-start min-100-vh">
       <Header />
-      <div className='container'>
+      <div className="container">
         <Home />
       </div>
       <Footer />
