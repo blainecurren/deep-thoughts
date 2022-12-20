1a38c14fcbd26b7ee4335ff5e29194744a38f325
Blaine Curren <blaine.curren@gmail.com>
2022-12-20 10:20:49 -0600

    client / server working

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
+  process.env.MONGODB_URI || "mongodb://localhost/deep-thoughts"
 );
 
 module.exports = mongoose.connection;
