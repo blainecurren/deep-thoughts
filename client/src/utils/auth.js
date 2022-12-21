65e0b7d2e6704237d861840708298c19362e22eb
Blaine Curren <blaine.curren@gmail.com>
2022-12-21 11:17:50 -0600

    added Auth functionality 'working'

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,20 +1,20 @@
 import decode from "jwt-decode";
 
 class AuthService {
-  //retrieve data saved in token
+  // retrieve data saved in token
   getProfile() {
     return decode(this.getToken());
   }
 
   // check if the user is still logged in
   loggedIn() {
-    //checks if there is a saved token and its still valid
+    // Checks if there is a saved token and it's still valid
     const token = this.getToken();
-    // use type coercion to check if token is NOT undefined and the token is NOT expired
+    // use type coersion to check if token is NOT undefined and the token is NOT expired
     return !!token && !this.isTokenExpired(token);
   }
 
-  // check if token is expired
+  // check if the token has expired
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
@@ -30,8 +30,13 @@ class AuthService {
 
   // retrieve token from localStorage
   getToken() {
+    // Retrieves the user token from localStorage
     return localStorage.getItem("id_token");
+  }
 
+  // set token to localStorage and reload page to homepage
+  login(idToken) {
+    // Saves user token to localStorage
     localStorage.setItem("id_token", idToken);
 
     window.location.assign("/");
@@ -39,7 +44,7 @@ class AuthService {
 
   // clear token from localStorage and force logout with reload
   logout() {
-    //clear user token and profile data from localStorage
+    // Clear user token and profile data from localStorage
     localStorage.removeItem("id_token");
     // this will reload the page and reset the state of the application
     window.location.assign("/");
