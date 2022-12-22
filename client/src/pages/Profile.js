08950d42b5f8858b0490fcfd049c61908548ff4c
Blaine Curren <blaine.curren@gmail.com>
2022-12-21 23:02:24 -0600

    added ThoughtForm to home and profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,6 +4,7 @@ import Auth from "../utils/auth";
 
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
+import ThoughtForm from "../components/ThoughtForm";
 
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
@@ -75,6 +76,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
